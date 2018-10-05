bff56557eeda4f81010dbcd58d2f9db8fd31c00e
Trevor Dowdle <tdowdle@wheelercat.com>
2018-10-05 15:57:32 -0600

    Step 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,6 +2,11 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">
+                {{book.title +":"+book.author}}
+            </li>
+        </ul>
     </div>
 </template>
 <script>
@@ -9,7 +14,12 @@
         name:'BookList',
         data:function(){
             return {
-                title:"All Books"
+                title:"All Books",
+                books:[
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
             }
         }    
     }; 
@@ -17,6 +27,14 @@
 <style>
     h1,h2 {
         font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>
 
